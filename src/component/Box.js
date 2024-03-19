778e0542f1e2743478a0177c0d9421205468bfae
park <kwon1341@github.com>
2024-03-20 03:31:44 +0900

    rcp-01

--- src/component/Box.js
+++ src/component/Box.js
@@ -6,6 +6,7 @@ const Box = (props) => {
         <div className="box">
             <h1>{props.title}</h1>
             <img className="item-img"
+                 alt="imgs"
                  src={props.item && props.item.img}
             />
             {/*<h2>{props.title}</h2>*/}
